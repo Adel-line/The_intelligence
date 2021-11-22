d55c7a14d81db7a694af42dddda9f6d91376ab83
marwane <marw0619@stud.kea.dk>
2021-11-22 15:56:24 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React yesyes your mom
+          Learn React yesyes your mom time two
         </a>
       </header>
     </div>
